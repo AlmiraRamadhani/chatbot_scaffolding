b4643e93b6daf57ee31cd115f84d5e7c911f2e0b
Joshua Davian <joshuadavian@gmail.com>
2020-06-20 16:38:07 +0700

    Transaksi

--- src/sewa/admin/transaksi_aksi.php
+++ src/sewa/admin/transaksi_aksi.php
@@ -5,11 +5,14 @@ $fdate = $_POST['fdate'];
 $ldate = $_POST['ldate'];
 $loanstatus = 0;
 $paidstatus = 0;
-mysqli_query($koneksi, "INSERT INTO tb_transaction values ('','$id_cust','$fdate','$ldate','$loanstatus','$paidstatus','')");
+mysqli_query($koneksi, "INSERT INTO tb_transaction values ('','$id_cust','$fdate','$ldate','$loanstatus','$paidstatus')");
+
 //tanggal
-$awal = date_create($fdate);
-$akhir = date_create($ldate);
-$hari = date_diff($awal, $akhir);
+// $awal = strtotime($fdate);
+// $akhir = strtotime($ldate);
+
+// $diff = $akhir - $awal;
+// $hari = round($diff / 86400);
 
 //$datetime1 = date_create('2017-06-28');
 // $datetime2 = date_create('2018-06-28');
@@ -20,21 +23,29 @@ $hari = date_diff($awal, $akhir);
 // echo $hari->format('%R%a days');
 //echo  var_dump($hari);
 //die();
-if ($hari <= 7) {
-    $query = mysqli_query($koneksi, "SELECT product_owp FROM tb_product where product_id=product_id");
-} elseif ($hari >= 8 && $hari <= 14) {
-    $query = mysqli_query($koneksi, "SELECT product_twp FROM tb_product where product_id=product_id");
-} elseif ($hari >= 15 && $hari <= 30) {
-    $query = mysqli_query($koneksi, "SELECT product_omp FROM tb_product where product_id=product_id");
-}
-//masuk db
+
 $last_id = mysqli_insert_id($koneksi);
-$barang = $_POST['product_name'];
-$price = mysqli_fetch_array($query);
-$qty = $_POST['quantity'];
+$barang = $_POST['barang'];
+// $price = $hrg['harga'];
+$qty = $_POST['qty'];
+
+// if ($hari <= 7) {
+//     $query = mysqli_query($koneksi, "SELECT product_owp AS harga FROM tb_product where product_id = product_id");
+// } elseif ($hari >= 8 && $hari <= 14) {
+//     $query = mysqli_query($koneksi, "SELECT product_twp AS harga FROM tb_product where product_id = product_id");
+// } elseif ($hari >= 15 && $hari <= 30) {
+//     $query = mysqli_query($koneksi, "SELECT product_omp AS harga FROM tb_product where product_id = product_id");
+// }
+
+//Convert hasil query ke array
+// $hrg = mysqli_fetch_array($query);
+
+//masuk db
+
+
 for ($i = 0; $i < count($barang); $i++) {
     if ($barang[$i] != "") {
-        mysqli_query($koneksi, "INSERT INTO tb_detail VALUES ('', '$last_id', '$barang[$i]', '$price[$i]', '$qty[$i]')");
+        mysqli_query($koneksi, "INSERT INTO tb_detail VALUES ('', '$last_id', '$barang[$i]', '$qty[$i]')");
     }
 }
 // $totalharga = $qty * product_owp
