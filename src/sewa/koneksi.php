0402b2e15060c712c5a327d19227da83b00d0d80
Almira Ramadhani <49553508+AlmiraRamadhani@users.noreply.github.com>
2020-07-18 18:03:11 +0700

    update terakhir file dokumen

--- src/sewa/koneksi.php
+++ src/sewa/koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$koneksi = mysqli_connect("localhost", "root", "", "db_sewa");
+$koneksi = mysqli_connect("localhost", "root", "", "penyewa2_db_sewa");
 
 if (mysqli_connect_errno()) {
 	echo "Koneksi database gagal : " . mysqli_connect_error();
